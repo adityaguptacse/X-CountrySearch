0d7f3ab73cdff0b7d7f1b61a15a95c7f7d1b5e0a
Aditya Gupta <adityaguptacse@gmail.com>
2024-04-26 23:15:08 -0700

    X country search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+
 
 function App() {
+  let [flagList, setFlagList] = useState([]);
+  let [cardList, setCardList] = useState([]);
+  let [searchText, setSearchText] = useState("");
+
+
+  useEffect(() => {
+    try {
+      fetch("https://restcountries.com/v3.1/all")
+        .then((res) => {
+          console.log("first res", res);
+          return res.json();
+        })
+        .then((res) => {
+          console.log("2nd res", res);
+          setFlagList(res);
+        })
+        .catch((err) => {
+          console.log("err-> ", err);
+        });
+    } catch (error) {
+      console.log("error in fetching",error);
+    }
+  }, []);
+
+  useEffect(() => {
+    let list = [];
+    console.log("searched Text", searchText);
+    if (flagList.length > 0 && searchText.length > 0) {
+      list = flagList.filter((ele) => {
+        //console.log(str.includes(substr));
+        let name = ele.name.common.toLowerCase();
+        if (name.includes(searchText.toLowerCase())) {
+          return ele;
+        }
+      });
+      console.log("filtered List-> ", list);
+      setFlagList(list);
+    } else {
+      try {
+        fetch("https://restcountries.com/v3.1/all")
+          .then((res) => {
+            return res.json();
+          })
+          .then((res) => {
+            console.log(res);
+            setFlagList(res);
+          })
+          .catch((err) => {
+            console.log("err-> ", err);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [searchText]);
+
+  useEffect(() => {
+    if (flagList.length) {
+      let cardList = flagList.map((ele) => {
+        return (
+          <div className="countryCard">
+            <img
+              src={ele.flags.png}
+              alt={ele.flags.alt}
+              width="100px"
+              height="100px"
+            />
+            <h2 className="ctyName">{ele.name.common}</h2>
+          </div>
+        );
+      });
+      setCardList(cardList);
+    }
+
+  }, [flagList]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <input
+          placeholder="Search for countries..."
+          type="text"
+          className="searchBox"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
       </header>
+      <div className="mainDiv">{cardList}</div>
     </div>
   );
 }
